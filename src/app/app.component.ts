a1948eb8922ff0e11a5546f3b064691f83f7e9a0
Mode Edna <89435658+ModeEdna@users.noreply.github.com>
2025-03-04 19:31:15 -0500

    added footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 // import header component
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [HeaderComponent], // Import the standalone component here
+  imports: [HeaderComponent, FooterComponent], // Import the standalone component here
 })
 export class AppComponent {
   title = 'VG_Website_Angular';
