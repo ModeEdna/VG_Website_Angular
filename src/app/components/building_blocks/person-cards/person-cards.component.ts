2b5a5b7914fc74409994d7ef2cc8e0e0808f04c4
Mode Edna <89435658+ModeEdna@users.noreply.github.com>
2025-03-06 10:25:19 -0500

    added people file

---
+++ src/app/components/building_blocks/person-cards/person-cards.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-person-cards',
+  imports: [],
+  templateUrl: './person-cards.component.html',
+  styleUrl: './person-cards.component.css'
+})
+export class PersonCardsComponent {
+
+}
