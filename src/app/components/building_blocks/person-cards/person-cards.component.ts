ccd80eead1d5ae3b3dc99c98f323a9ce94e1ec5f
Mode Edna <89435658+ModeEdna@users.noreply.github.com>
2025-03-06 10:50:12 -0500

    checkpoint prior to asking agent to mess with stuff

--- src/app/components/building_blocks/person-cards/person-cards.component.ts
+++ src/app/components/building_blocks/person-cards/person-cards.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-person-cards',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './person-cards.component.html',
-  styleUrl: './person-cards.component.css'
+  styleUrl: './person-cards.component.css',
 })
-export class PersonCardsComponent {
+export class PersonCardsComponent implements OnInit {
+  people: any[] = [];
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<any[]>('/assets/json_files/people.json').subscribe((data) => {
+      this.people = data;
+    });
+  }
 }
