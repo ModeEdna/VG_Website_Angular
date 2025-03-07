6dcc96686fde7dce0f39cacdd05bc132f28a622f
Mode Edna <89435658+ModeEdna@users.noreply.github.com>
2025-03-07 11:00:44 -0500

    created learning resources json and a component for learning cards

---
+++ src/app/components/building_blocks/learn-card/learn-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-learn-card',
+  imports: [],
+  templateUrl: './learn-card.component.html',
+  styleUrl: './learn-card.component.css'
+})
+export class LearnCardComponent {
+
+}
