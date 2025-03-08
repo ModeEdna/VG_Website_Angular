23248e2052636efcc5265c79f69fbf8c6bed72cf
Mode Edna <89435658+ModeEdna@users.noreply.github.com>
2025-03-08 16:22:26 -0500

    policies page isn't working

--- src/app/components/building_blocks/policies-table/policies-table.component.ts
+++ src/app/components/building_blocks/policies-table/policies-table.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-policies-table',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './policies-table.component.html',
-  styleUrl: './policies-table.component.css'
+  styleUrl: './policies-table.component.css',
 })
-export class PoliciesTableComponent {
+export class PoliciesTableComponent implements OnInit {
+  policies: any[] = [];
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get<{ policies: any[] }>('/json_files/policies.json').subscribe({
+      next: (data) => {
+        this.policies = data.policies || [];
+      },
+      error: (err) => console.error(err),
+    });
+  }
 }
--- src/app/components/pages/policies-and-procedures/policies-and-procedures.component.ts
+++ src/app/components/pages/policies-and-procedures/policies-and-procedures.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { PoliciesTableComponent } from '../../building_blocks/policies-table/policies-table.component';
 
 @Component({
   selector: 'app-policies-and-procedures',
-  imports: [],
+  imports: [PoliciesTableComponent],
   templateUrl: './policies-and-procedures.component.html',
-  styleUrl: './policies-and-procedures.component.css'
+  styleUrl: './policies-and-procedures.component.css',
 })
-export class PoliciesAndProceduresComponent {
-
-}
+export class PoliciesAndProceduresComponent {}
