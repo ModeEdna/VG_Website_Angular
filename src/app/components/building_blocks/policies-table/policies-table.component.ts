6aebb1ebd9805c847c57e6224636ee8d445a8cea
Mode Edna <89435658+ModeEdna@users.noreply.github.com>
2025-03-08 16:01:59 -0500

    created policies table component

---
+++ src/app/components/building_blocks/policies-table/policies-table.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-policies-table',
+  imports: [],
+  templateUrl: './policies-table.component.html',
+  styleUrl: './policies-table.component.css'
+})
+export class PoliciesTableComponent {
+
+}
