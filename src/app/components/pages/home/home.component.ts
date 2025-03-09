5157c38b816637dbe274ee63a7471e05c2c092da
Mode Edna <89435658+ModeEdna@users.noreply.github.com>
2025-03-09 00:15:22 -0500

    starting home page

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { CarouselComponent } from '../../building_blocks/carousel/carousel.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CarouselComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
