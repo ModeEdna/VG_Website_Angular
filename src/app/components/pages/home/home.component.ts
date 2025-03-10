e2f37b8cd598e2b105584c6095a2b6744a68d2f3
Mode Edna <89435658+ModeEdna@users.noreply.github.com>
2025-03-10 10:10:45 -0400

    fixed table buttons colors

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CarouselComponent } from '../../building_blocks/carousel/carousel.component';
 import { LogoCardComponent } from '../../building_blocks/logo-card/logo-card.component';
+import { AdminCardComponent } from '../../building_blocks/admin-card/admin-card.component';
 
 @Component({
   selector: 'app-home',
-  imports: [CarouselComponent, LogoCardComponent],
+  imports: [CarouselComponent, LogoCardComponent, AdminCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
